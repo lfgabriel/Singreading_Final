20773f7ed64e03c94f72f2d1005a1f291aa11c4b
lsitec219.franco <gabriel.franco@lsitec.org.br>
2018-04-13 13:11:09 -0300

    Favorite and unfavorite correctly

--- app/src/main/java/com/singreading/DetailActivity.java
+++ app/src/main/java/com/singreading/DetailActivity.java
@@ -3,6 +3,7 @@ package com.singreading;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -11,8 +12,11 @@ import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.singreading.model.Lyric;
 import com.singreading.model.User;
 
@@ -29,6 +33,8 @@ public class DetailActivity extends AppCompatActivity {
 
     private TextView lyricTextView;
     private TextView detailTitle;
+    FloatingActionButton fab;
+    boolean isFavorite;
 
     private DatabaseReference mDatabase;
     FirebaseUser firebaseUser;
@@ -41,7 +47,7 @@ public class DetailActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -57,28 +63,74 @@ public class DetailActivity extends AppCompatActivity {
                 + " - " + lyric.getName().substring(0, 1).toUpperCase() + lyric.getName().substring(1));
 
         lyricTextView = (TextView) findViewById(R.id.tv_lyric);
+
+        //GET USER AND DATABASE
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
 
         lyricTextView.setText(lyric.getAllLyric());
-
-
     }
 
-    public void favorite(Lyric lyric, View view)
-
-    {
-        List<Lyric> userLyrics = new ArrayList<Lyric>();
-        userLyrics.add(lyric);
+    @Override
+    protected void onStart() {
+        super.onStart();
 
         mDatabase = FirebaseDatabase.getInstance().getReference("lyrics")
                 .child(firebaseUser.getUid());
 
-        mDatabase.child(String.valueOf(lyric.getId())).setValue(lyric);
+        final List<Lyric> lyrics = new ArrayList<Lyric>();
+        mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                lyrics.clear();
 
-        Snackbar.make(view, "Added to the favorites!", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show();
+                if (dataSnapshot.getValue() != null) {
 
-        Log.e(TAG, "Lyric saved");
+                    for (DataSnapshot lyricSnapshot : dataSnapshot.getChildren()) {
+                        Lyric lyricFromFavorites = lyricSnapshot.getValue(Lyric.class);
+                        if (lyricFromFavorites.getId().equals(lyric.getId())) {
+                            Log.e(TAG, lyric.getId() + "IS FAVORITE!! ");
+                            fab.setImageDrawable(ContextCompat.getDrawable(getBaseContext(), R.drawable.favorite_star_yellow));
+                            isFavorite = true;
+                            return;
+                        }
+                    }
+                    fab.setImageDrawable(ContextCompat.getDrawable(getBaseContext(), R.drawable.favorite_star));
+                    isFavorite = false;
+
+                }
+                else {
+                    Log.e(TAG, "Empty favorites: ");
+                    fab.setImageDrawable(ContextCompat.getDrawable(getBaseContext(), R.drawable.favorite_star));
+                    isFavorite = false;
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Failed to read value
+                Log.e(TAG, "Failed to read value.", databaseError.toException());
+            }
+        });
+    }
+
+    public void favorite(Lyric lyric, View view) {
+        mDatabase = FirebaseDatabase.getInstance().getReference("lyrics")
+                .child(firebaseUser.getUid());
+
+        if (isFavorite) {
+            mDatabase.child(String.valueOf(lyric.getId())).setValue(null);
+            fab.setImageDrawable(ContextCompat.getDrawable(getBaseContext(), R.drawable.favorite_star));
+            isFavorite = false;
+        }
+        else {
+            mDatabase.child(String.valueOf(lyric.getId())).setValue(lyric);
+
+            Snackbar.make(view, "Added to the favorites!", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+
+            fab.setImageDrawable(ContextCompat.getDrawable(getBaseContext(), R.drawable.favorite_star_yellow));
+            isFavorite = true;
+        }
     }
 
 }
